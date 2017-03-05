8897107e8d1e83891dd0a6f08a6f5f898b0d5fb4
tundraka <tundraka@gmail.com>
2017-03-05 14:59:25 -0600

    typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let coordinates = new Geolocation();
 coordinates.getGeoLocation().then((coordinates) => {
         console.log(`the coordinates are ${coordinates}`);
         var mymap = L.map('mapid').setView(
-            [coordinate.latitude, coordinates.longitude], 13);
+            [coordinates.latitude, coordinates.longitude], 13);
     }).catch((error) => {
         console.log(error);
     });
