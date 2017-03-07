c94dc114451bace1cc17c6fb2a53cbc1787dba88
tundraka <tundraka@gmail.com>
2017-03-07 13:57:36 -0600

    Cleaning.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const geo = new Geolocation();
+const map = L.map('mapid');
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
 geo.getGeoLocation().then((coordinates) => {
-    var mymap = L.map('mapid').setView(
-        [coordinates.latitude, coordinates.longitude], 15);
+    const location = [coordinates.latitude, coordinates.longitude];
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
+    map.setView(location, 15);
 
     //L.marker([coordinates.latitude, coordinates.longitude]).addTo(mymap)
     //.bindPopup('You\'re here!')
     //.openPopup();
 
-    var circle = L.circle([coordinates.latitude, coordinates.longitude], {
+    let circle = L.circle(location, {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.3,
-        radius: 300}).addTo(mymap);
+        radius: 300
+    }).addTo(map);
 }).catch((error) => {
     console.log(error);
 });
