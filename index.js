fdf2eea7ded3725d7ecb8c4478eeef5de5fe6dde
tundraka <tundraka@gmail.com>
2017-03-06 22:35:52 -0600

    Adding some conf to leaflet

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let coordinates = new Geolocation();
-coordinates.getGeoLocation().then((coordinates) => {
-        console.log(`the coordinates are ${coordinates}`);
-        var mymap = L.map('mapid').setView(
-            [coordinates.latitude, coordinates.longitude], 13);
-    }).catch((error) => {
-        console.log(error);
-    });
+const geo = new Geolocation();
+
+geo.getGeoLocation().then((coordinates) => {
+    var mymap = L.map('mapid').setView(
+        [coordinates.latitude, coordinates.longitude], 15);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
+    //L.marker([coordinates.latitude, coordinates.longitude]).addTo(mymap)
+    //.bindPopup('You\'re here!')
+    //.openPopup();
+
+    var circle = L.circle([coordinates.latitude, coordinates.longitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.3,
+        radius: 300}).addTo(mymap);
+}).catch((error) => {
+    console.log(error);
+});
